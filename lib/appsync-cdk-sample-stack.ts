79830a4ea3c45e256eab1bda1ac758c2526e6281
tokku5552 <physics.0121@gmail.com>
2023-02-05 15:04:36 +0900

    add: appsync and ddb

--- lib/appsync-cdk-sample-stack.ts
+++ lib/appsync-cdk-sample-stack.ts
@@ -1,16 +1,53 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class AppsyncCdkSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: "Todo",
+      schema: appsync.SchemaFile.fromAsset(
+        path.join(__dirname, "../schemas/schema.graphql")
+      ),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncCdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, "TodoTable", {
+      tableName: "todo-table",
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    const dataSource = api.addDynamoDbDataSource("DynamoDataSource", table);
+
+    // query
+    dataSource.createResolver("QueryResolver", {
+      typeName: "Query",
+      fieldName: "getTodos",
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList(),
+    });
+
+    // mutation
+    dataSource.createResolver("MutationResolver", {
+      typeName: "Mutation",
+      fieldName: "addTodo",
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+        appsync.PrimaryKey.partition("id").auto(),
+        appsync.Values.projecting("input")
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
   }
 }
