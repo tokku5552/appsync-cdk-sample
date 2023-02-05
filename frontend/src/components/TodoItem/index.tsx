7dbeb7d626f93e9f2c55fe78594326770e134172
tokku5552 <physics.0121@gmail.com>
2023-02-05 22:08:00 +0900

    add: delete処理

--- frontend/src/components/TodoItem/index.tsx
+++ frontend/src/components/TodoItem/index.tsx
@@ -1,8 +1,10 @@
 import {
   Todo,
   UpdateInput,
+  useDeleteMutation,
   useUpdateMutation,
 } from "@/types/generated/generated-types";
+import { DeleteIcon } from "@chakra-ui/icons";
 import { Card, CardBody, Checkbox, HStack, Text } from "@chakra-ui/react";
 import { useCallback, useState } from "react";
 
@@ -11,23 +13,35 @@ export interface TodoItemProps {
 }
 
 export default function TodoItem({ todo }: TodoItemProps) {
-  const [todoItem, setTodoItem] = useState(todo);
-  const [updateMutation, { data, loading, error }] = useUpdateMutation();
+  const [todoItem, setTodoItem] = useState<Todo | null>(todo);
+  const [updateMutation, updateState] = useUpdateMutation();
+  const [deleteMutation, deleteState] = useDeleteMutation();
 
   const handleCheck = useCallback(() => {
-    if (loading) return;
+    if (updateState.loading) return;
     const updatedTodoItem: UpdateInput = {
-      id: todoItem.id,
-      isDone: !todoItem.isDone,
+      id: todoItem!.id,
+      isDone: !todoItem!.isDone,
     };
 
     updateMutation({
       variables: { input: updatedTodoItem },
       onCompleted: () => {
-        setTodoItem({ ...todoItem, isDone: updatedTodoItem.isDone! });
+        setTodoItem({ ...todoItem!, isDone: updatedTodoItem.isDone! });
       },
     });
-  }, [loading, todoItem, updateMutation]);
+  }, [updateState, todoItem, updateMutation]);
+
+  const handleDelete = useCallback(() => {
+    if (!todoItem) return;
+    if (deleteState.loading) return;
+    deleteMutation({
+      variables: { input: todoItem.id },
+      onCompleted: () => setTodoItem(null),
+    });
+  }, [deleteState, deleteMutation, todoItem]);
+
+  if (!todoItem) return null;
 
   return (
     <>
@@ -41,6 +55,12 @@ export default function TodoItem({ todo }: TodoItemProps) {
                 onChange={handleCheck}
               />
               <Text>{todoItem.title}</Text>
+              <DeleteIcon
+                color="blue.500"
+                boxSize={5}
+                _hover={{ boxSize: 6 }}
+                onClick={handleDelete}
+              />
             </HStack>
           </CardBody>
         </Card>
